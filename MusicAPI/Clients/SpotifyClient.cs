867fc54e54e15ba06faafb8a23dec84edddf2815
Maria <mariayurch@icloud.com>
2023-06-16 15:22:09 +0300

    new commit

---
+++ MusicAPI/Clients/SpotifyClient.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using MusicAPI.Models;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Http;
+
+namespace MusicAPI.Clients
+{
+    public class SpotiClient
+    {
+        private static string _secretClient = "643c092c22174852a6530ec0fc920402";
+        private static string _clientID = "3c1d8271e3c84c9ea6018d8b00c436b4";
+
+        private static readonly HttpClient _client = new ();
+        private static readonly SpotifyClient _spotifyClient = new (GetAccessToken().Result);
+
+        private static async Task<string> GetAccessToken()
+        {
+            var base64Credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_clientID}:{_secretClient}"));
+
+            _client.DefaultRequestHeaders.Add("Authorization", $"Basic {base64Credentials}");
+            _client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            });
+            var response = await _client.PostAsync("https://accounts.spotify.com/api/token", content);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var accessToken = JsonConvert.DeserializeObject<AccessToken>(responseContent);
+            var token = accessToken.Access_token;
+
+            return token;
+        }
+
+        public async Task<FullPlaylist> GetPlaylist(string playlistId)
+        {
+            var playlist = await _spotifyClient.Playlists.Get(playlistId);
+            return playlist;
+        }
+
+        public async Task<PrivateUser> GetCurrentUser()
+        {
+            var user = await _spotifyClient.UserProfile.Current();
+            return user;
+        }
+
+        public string SendAuthorizationLink()
+        {
+            string botUrl = "https://t.me/RtmXBot";
+            string scopes = "playlist-read-private " +
+                            "playlist-read-collaborative " +
+                            "playlist-modify-private " +
+                            "playlist-modify-public " +
+                            "user-top-read " +
+                            "user-library-modify " +
+                            "user-library-read " +
+                            "user-read-email " +
+                            "user-read-private";
+            return $"https://accounts.spotify.com/authorize?client_id=" +
+                   $"{_clientID}&response_type=code&" +
+                   $"redirect_uri={botUrl}&" +
+                   $"scope={scopes}";
+        }
+    }
+}
---
+++ TelegramBot/Handlers/MusicApiHandler.cs
@@ -0,0 +1,42 @@
+﻿using SpotifyAPI.Web;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot.Handlers
+{
+    internal static class MusicApiHandler
+    {
+        private static HttpClient _musicClient = new()
+        {
+            BaseAddress = new Uri("https://localhost:7123/")
+        };
+
+#if CUSTOM_AWAIT
+        public static T Await<T>(Task<T> task)
+        {
+            task.Wait();
+            return task.Result;
+        }
+#endif
+        public static async Task ConnectSpotify(ITelegramBotClient botClient, CallbackQuery? callbackQuery)
+        {
+            var response = await _musicClient.GetAsync("api/spotify/get-current-user");
+            if (response.GetType() != typeof(PrivateUser))
+            {
+                await botClient.SendTextMessageAsync(callbackQuery.Message.Chat, "You have to authorize",
+                    replyMarkup: new InlineKeyboardMarkup(new[]
+                    {
+                        InlineKeyboardButton.WithUrl(
+                            text: "Authorize me",
+                            url: await response.Content.ReadAsStringAsync())
+                    }));
+            }
+            else if (response.GetType() != typeof(string))
+            {
+                await botClient.SendTextMessageAsync(callbackQuery.Message.Chat,
+                    "That's it. You connected your Spotify account.");
+            }
+        }
+    }
+}
